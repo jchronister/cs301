64def40e0fcc50ffd757c606343dee1add5f8da1
jchronister <69609933+jchronister@users.noreply.github.com>
2020-08-13 10:22:23 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+
+function msgName (name){
+    alert(name);
+}
+
+function sumMe (number){
+    var total=0, numberInt = parseInt(number,10);
+    for(var i = 1;i <= numberInt;i+=1){
+      total += i;
+    }
+    return total;
+}
+
+console.log(sumMe(10));
