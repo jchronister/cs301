a2d0b171150f1e203f24d37213631e319f2b5655
jchronister <69609933+jchronister@users.noreply.github.com>
2020-08-20 16:04:57 -0500

    Assignment 3

--- Assignment3/Chapter6_Question1.js
+++ Assignment3/Chapter6_Question1.js
@@ -4,15 +4,15 @@
 Defining Table
 Input                   Processing                  Output
 User Enter              Sort Numbers in Order       Absolute Difference to User
-Number 1                Calulate Difference
-Number 2                Based on Reference to 0
+Number 1                Subtract Largest from
+Number 2                Smallest
+
 */
 
 let prompt = require('prompt-sync')();
 
 let num1 = parseFloat(prompt('Please Enter Number 1: '));
 let num2 = parseFloat(prompt('Please Enter Number 2: '));
-let  = 0;
 
 // Verify Num2 > Num1
 if (num2 < num1) {
@@ -21,24 +21,10 @@ if (num2 < num1) {
     num2 = temp;
 }
 
-// if (num2 < 0) {
-//     // Both < 0
-//     //diff = Math.abs(num1 - num2);
-//     diff = -num1 + num2 // Same as Line Above
-// } else if (num1 > 0) {
-//     // Both > 0
-//     diff = num2 - num1;
-// } else {
-//     // Split 0
-//     diff = -num1 + num2;
-// }
-// Which Reduces to ...
-
-if (num1 > 0) {
-    // Both > 0
-    diff = num2 - num1;
-} else {
-    diff = -num1 + num2;
-}
+diff = num2 - num1
 
 console.log('The Absolute Difference of ' + num1 + ' and ' + num2 + ' is ' +diff);
+
+
+
+
